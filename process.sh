b6a00968c04e0f4352732b0610929bb2658a85f3
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2020-12-06 15:56:31 +0000

    Update process.sh

--- process.sh
+++ process.sh
@@ -17,8 +17,9 @@ export SCRIPTS_DIR=$WORKSPACE/scripts
 
 echo "** Creating temporary directories.. **"
 cd ${WORKSPACE}
-rm -rf $FINAL_DUMPS_DIR $RAW_DUMPS_DIR
-mkdir $FINAL_DUMPS_DIR $RAW_DUMPS_DIR
+find $FINAL_DUMPS_DIR -type f -delete
+find $RAW_DUMPS_DIR -type f -delete
+mkdir -p $FINAL_DUMPS_DIR $RAW_DUMPS_DIR
 
 echo "VFBTIME:"
 date
