f5435e053a080e1be663c69ebea9b3ff91cc02ef
Rob Court <rcourt@ed.ac.uk>
2024-07-18 23:44:59 +0100

    ensuring all output passed

--- process.sh
+++ process.sh
@@ -27,7 +27,7 @@ date
 
 echo '** Executing pipeline.. **' | tee ${OUTDIR}/dumps.log
 
-{ /usr/bin/time -v make all ; } 2> ${OUTDIR}/dumps.log
+{ /usr/bin/time -v make all ; } 2>&1 | tee -a ${OUTDIR}/dumps.log
 
 echo "End: vfb-pipeline-dumps"
 echo "VFBTIME:"
