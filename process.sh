60dae499218cd990664412677b074b375e80c431
Rob Court <rcourt@ed.ac.uk>
2024-08-23 10:04:11 +0100

    testing with 10 cores

--- process.sh
+++ process.sh
@@ -27,7 +27,7 @@ date
 
 echo '** Executing pipeline.. **' | tee ${OUTDIR}/dumps.log
 
-{ /usr/bin/time -v make -d -j2 all ; } 2>&1 | tee -a ${OUTDIR}/dumps.log
+{ /usr/bin/time -v make -d -j10 all ; } 2>&1 | tee -a ${OUTDIR}/dumps.log
 
 echo "End: vfb-pipeline-dumps"
 echo "VFBTIME:"
