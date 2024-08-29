5001a460d819a2508087c6443f1f1897b40729b5
Rob Court <rcourt@ed.ac.uk>
2024-08-29 18:08:45 +0100

    Revert "buildinng debug first"
    This reverts commit 2b65834698e35e91d91cfff811b070ceae3bf075.

--- process.sh
+++ process.sh
@@ -27,9 +27,6 @@ date
 
 echo '** Executing pipeline.. **' | tee ${OUTDIR}/dumps.log
 
-make print_pdb_external_onts
-make print_query_outputs
-
 { /usr/bin/time -v make -d -j${CORES} all ; } 2>&1 | tee -a ${OUTDIR}/dumps.log
 
 echo "End: vfb-pipeline-dumps"
